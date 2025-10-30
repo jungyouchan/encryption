6cfb27ecdc9e088c01494ed8d874c1187692b110
yuchan <yuchan613@naver.com>
2025-10-30 11:11:34 +0900

    update fetch url to real domain

--- hash/src/main.js
+++ hash/src/main.js
@@ -1,8 +1,8 @@
 document.onload = function () {
   const rainbowTable = document.getElementById("rainbow-table");
   const rainbowTableExplain = document.getElementById("rainbow-table-explain");
-  rainbowTable.addEventListener("mouseover", function () {
-    rainbowTableExplain.style.visibility = "visible";
-    
+  rainbowTable.addEventListener("mouseover", (e) => {
+    rainbowTableExplain.style.display = "block";
+    rainbowTableExplain.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
   });
 }
