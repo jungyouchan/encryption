dda3cfdd98e141142baab084b0a660f2df464f9c
yuchan <yuchan613@naver.com>
2025-11-03 09:10:44 +0900

    data connection

--- hash/api/submit.js
+++ hash/api/submit.js
@@ -15,8 +15,7 @@ export default function handler(req, res) {
   if (req.method === 'POST') {
     // 클라이언트에서 보낸 데이터를 받기
     const data = req.body;
-    hashData.push(data.receivedData.passwordHashes); //hashData = [ {SHA256:..., Argon2:..., Bcrypt:...}, {...}, ... ]
-
+    hashData.push(data.passwordHashes); //hashData = [ {SHA256:..., Argon2:..., Bcrypt:...}, {...}, ... ]
     console.log("서버가 받은 데이터:", data);
 
     // 받은 데이터를 기반으로 응답을 보냄
@@ -25,21 +24,25 @@ export default function handler(req, res) {
       message: `${data.id} 데이터가 정상 처리되었습니다`,  // 받은 데이터의 id를 활용
       receivedData: data
     });
+
+    return;
   } else {
     res.status(405).json({ error: 'Method Not Allowed' }); // 허용되지 않은 HTTP 메서드에 대한 응답
   }
-
+  
   if (req.method === 'GET') {
     const data = {
       message: "서버에서 받은 데이터입니다.",
       receivedData: {
-        id: "user1",
         passwordHashes: hashData
       }
     };
 
+    hashData = []; // 데이터를 보낸 후에는 초기화
+
     res.status(200).json(data);
 
+    return;
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
   }
